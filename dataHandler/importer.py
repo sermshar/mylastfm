050b73062f80da7655e8314ac0e449847a60a605
Stephen Ermshar <stephen.ermshar@gmail.com>
2018-12-26 23:52:56 -0800

    generalize importer.py to be called from the outside

--- dataHandler/importer.py
+++ dataHandler/importer.py
@@ -1,42 +1,41 @@
-from    pylast_config   import NETWORK, USERNAME
-from    models          import Base, Scrobble
-from    sqlalchemy      import create_engine
-from    sqlalchemy.orm  import sessionmaker
-from    sqlalchemy.sql  import func
+from    dataHandler.models  import Base, Scrobble
+from    sqlalchemy          import create_engine
+from    sqlalchemy.orm      import sessionmaker
+from    sqlalchemy.sql      import func
 
-def updateDB():
+def updateDB(db_file, fm_network, fm_username):
     """
     Connect to or create the database, get recent tracks not already in the database and add them to it
     """
-    engine = create_engine('sqlite:///mylastfm.sqlite3', echo=False)
+    engine = create_engine(('sqlite:///' + db_file), echo=False)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    from_timestamp = last_timestamp(session) + 1
-    recent_scrobbles = get_scrobbles(from_timestamp)
+    from_timestamp = __last_timestamp(session) + 1
+    recent_scrobbles = __get_scrobbles(from_timestamp, fm_network, fm_username)
     session.bulk_save_objects(recent_scrobbles)
 
     session.commit()
     session.close()
 
 
-def get_scrobbles(from_timestamp):
+def __get_scrobbles(from_timestamp, fm_network, fm_username):
     """
     Get all scrobbles from last.fm starting from the last one in the database.
     If getAll is true, retrieve all scrobbles, 
     reseting the database to match last.fm (may take a few minutes).
     returns an array of Scrobble objects
     """
-    lfm_user = NETWORK.get_user(USERNAME)
+    lfm_user = fm_network.get_user(fm_username)
     cacheable = True if (from_timestamp == None) else False
     scrobbles = lfm_user.get_recent_tracks(
         limit=None, cacheable=cacheable, time_from=from_timestamp, time_to=None
         )
-    return format_scrobble_array(scrobbles=scrobbles)
+    return __format_scrobble_array(scrobbles=scrobbles)
     
 
-def format_scrobble_array(scrobbles):
+def __format_scrobble_array(scrobbles):
     """
     Convert the python list of scrobbles from pylast to a single dimensional array
     of Scrobble objects that can be mapped to the scrobble database.
@@ -52,6 +51,6 @@ def format_scrobble_array(scrobbles):
     return scrobble_array
 
 
-def last_timestamp(session):
+def __last_timestamp(session):
     """return the greatest (most recent) timestamp in the scrobble library"""
     return session.query(func.max(Scrobble.timestamp.label('max_timestamp'))).one()[0]
