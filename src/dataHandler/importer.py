f674e7979afb1f78514ab69e60d833f87835f0c7
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-12-27 01:28:22 -0800

    add new tables, small_session; refactor code

--- src/dataHandler/importer.py
+++ src/dataHandler/importer.py
@@ -1,61 +1,72 @@
-from src.dataHandler.models import Base, Scrobble
-from sqlalchemy import create_engine
+from src.dataHandler.models import Base, RawScrobble
+from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
+from contextlib import contextmanager
 
 
 def update_db(db_file, fm_network, fm_username):
     """
     Connect to or create the database, get recent tracks not already in the database and add them to it
     """
+    with small_session(db_file) as session:
+        last_raw_timestamp = __last_raw_timestamp(session)
+        from_timestamp = __last_raw_timestamp(session) + 1 if (last_raw_timestamp is not None) else None
+        recent_scrobbles = __get_raw_scrobbles(from_timestamp, fm_network, fm_username)
+        session.bulk_save_objects(recent_scrobbles)
+
+
+@contextmanager
+def small_session(db_file):
+    # https://docs.python.org/2.5/whatsnew/pep-343.html#module-contextlib
     engine = create_engine(("sqlite:///" + db_file), echo=False)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    from_timestamp = __last_timestamp(session) + 1
-    recent_scrobbles = __get_scrobbles(from_timestamp, fm_network, fm_username)
-    session.bulk_save_objects(recent_scrobbles)
+    try:
+        yield session
+        session.commit()
+    except exc.SQLAlchemyError as error:
+        session.rollback()
+        print("Encountered an SQLAlchemy Error, the session has been rolled back.")
+    finally:
+        session.close()
 
-    session.commit()
-    session.close()
 
-
-def __get_scrobbles(from_timestamp, fm_network, fm_username):
+def __get_raw_scrobbles(from_timestamp, fm_network, fm_username):
     """
-    Get all scrobbles from last.fm starting from the last one in the database.
-    If getAll is true, retrieve all scrobbles, 
-    resetting the database to match last.fm (may take a few minutes).
+    Get all scrobbles from last.fm starting from the last one in the database,
+    or all scrobbles if the database is empty (this may take a few minutes).
     returns an array of Scrobble objects
     """
     lfm_user = fm_network.get_user(fm_username)
     cacheable = True if (from_timestamp is None) else False
-    scrobbles = lfm_user.get_recent_tracks(
+    raw_scrobbles = lfm_user.get_recent_tracks(
         limit=None, cacheable=cacheable, time_from=from_timestamp, time_to=None
     )
-    return __format_scrobble_array(scrobbles=scrobbles)
+    return __format_raw_scrobble_array(raw_scrobbles=raw_scrobbles)
 
 
-def __format_scrobble_array(scrobbles):
+def __format_raw_scrobble_array(raw_scrobbles):
     """
     Convert the python list of scrobbles from pylast to a single dimensional array
-    of Scrobble objects that can be mapped to the scrobble database.
-
-    Note: timestamp is a unix timestamp (seconds since 1970-01-01 UTC)
+    of RawScrobble objects that can be mapped to the scrobble database.
+    Note that the timestamp is a unix timestamp (seconds since 1970-01-01 UTC)
     """
     scrobble_array = []
-    for row in scrobbles:
+    for row in raw_scrobbles:
         timestamp = row.timestamp.__str__()
         track = row.track.get_title().__str__()
         album = row.album.__str__()
         artist = row.track.get_artist().__str__()
-        scrobble = Scrobble(
+        raw_scrobble = RawScrobble(
             timestamp=timestamp, track=track, album=album, artist=artist
         )
-        scrobble_array.append(scrobble)
+        scrobble_array.append(raw_scrobble)
     return scrobble_array
 
 
-def __last_timestamp(session):
-    """return the greatest (most recent) timestamp in the scrobble library"""
-    return session.query(func.max(Scrobble.timestamp.label("max_timestamp"))).one()[0]
+def __last_raw_timestamp(session):
+    """return the greatest (most recent) timestamp in the RawScrobble table"""
+    return session.query(func.max(RawScrobble.timestamp.label("max_timestamp"))).one()[0]
--- src/dataHandler/models.py
+++ src/dataHandler/models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class Scrobble(Base):
-    __tablename__ = 'scrobbles'
+class RawScrobble(Base):
+    __tablename__ = "raw_scrobbles"
 
     id = Column(Integer, primary_key=True)
     timestamp = Column(Integer)
@@ -14,5 +14,42 @@ class Scrobble(Base):
     artist = Column(String)
 
     def __repr__(self):
-        return "<Scrobble(timestamp='%s', track='%s', album='%s', 'artist='%s')>" % (
+        return "<RawScrobble(timestamp='%s', track='%s', album='%s', 'artist='%s')>" % (
             self.timestamp, self.track, self.album, self.artist)
+
+
+class Track(Base):
+    __tablename__ = "rename_tracks"
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(Integer)
+    track = Column(String)
+    album = Column(String)
+    artist = Column(String)
+    isrc = Column(String)
+
+    def __repr__(self):
+        return "<RewriteScrobble(timestamp='%s', track='%s', album='%s', 'artist='%s')>" % (
+            self.timestamp, self.track, self.album, self.artist)
+
+
+class RewriteRule(Base):
+    __tablename__ = "rewrite_rules"
+
+    id = Column(Integer, primary_key=True)
+    raw_id = Column(Integer)
+    raw_timestamp = Column(Integer)
+    raw_track = Column(String)
+    raw_album = Column(String)
+    raw_artist = Column(String)
+    rename_track_id = Column(Integer)
+
+    # https://stackoverflow.com/a/10061143
+    __table_args__ = (UniqueConstraint(
+        'raw_id',
+        'raw_timestamp',
+        'raw_track',
+        'raw_album',
+        'raw_artist',
+        name='_customer_location_uc'),
+    )
